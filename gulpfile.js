583b1457af374df8087ff4d78648c38ee47637e0
PedroCaaveiro <acaaveir@yahoo.es>
2025-03-17 17:52:27 +0100

    cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,37 @@
-import {src, dest,watch, series} from 'gulp'
-import *  as dartSaas from 'sass'
-import gulpSass from 'gulp-sass'
- 
- const sass = gulpSass(dartSaas);
+import { src, dest, watch, series } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
- export function js(done){
+const sass = gulpSass(dartSass);
 
-   src('src/js/script.js')
-   .pipe(dest('dist/js'))
-   done()
- }
- export function css(done){
-    src('src/scss/app.scss', {sourcemaps: true})
-    .pipe(sass().on('error',sass.logError))
-    .pipe(dest('dist/css', {sourcemaps:true}))
-    
-    done();
- }
- export function dev(){
-watch('src/scss/**/*.scss',css)
-watch('src/js/**/*.js',js)
-
- }
- export function build(done) {
-  series(js, css)(done); // Ejecuta primero js y luego css
+export function js(done) {
+  src('src/js/script.js')
+    .pipe(dest('dist/js'));
+  done();
 }
 
- export default series(build, dev);
+export function css(done) {
+  src('src/scss/app.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css', { sourcemaps: true }));
+  done();
+}
+
+// Nueva tarea para copiar los archivos HTML
+export function html(done) {
+  src('src/*.html')  // Ajusta la ruta si tienes una estructura diferente
+    .pipe(dest('dist/'));  // Copia los archivos HTML a la carpeta 'dist'
+  done();
+}
+
+export function dev() {
+  watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', js);
+  watch('src/*.html', html);  // Observa los cambios en los archivos HTML
+}
+
+export function build(done) {
+  series(js, css, html)(done); // Ejecuta js, css y html
+}
+
+export default series(build, dev);
